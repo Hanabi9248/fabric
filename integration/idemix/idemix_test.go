04c1d69f905cf50cf27933d7f709d7998fc8b017
Hanabi <317387557+Hanabi9248@users.noreply.github.com>
2026-09-13 06:48:48 +0800

    test: use the channel-level capability update helper
    Signed-off-by: Hanabi <317387557+Hanabi9248@users.noreply.github.com>
    
    Signed-off-by: Hanabi <317387557+Hanabi9248@users.noreply.github.com>

--- integration/idemix/idemix_test.go
+++ integration/idemix/idemix_test.go
@@ -106,7 +106,10 @@ var _ = Describe("EndToEnd", func() {
 			Query(network, peer, "testchannel", "80")
 
 			By("upgrading the channel capability to V3_0")
-			nwo.EnableCapabilities(network, "testchannel", "Channel", "V3_0", orderer, network.Peer("Org1", "peer0"), network.Peer("Org2", "peer0"))
+			nwo.EnableChannelCapabilities(
+				network, "testchannel", "V3_0", false, orderer, []*nwo.Orderer{orderer},
+				network.Peer("Org1", "peer0"), network.Peer("Org2", "peer0"),
+			)
 
 			By("using the existing Idemix identity after the upgrade")
 			InvokeWithIdemix(network, orderer, peer, idemixOrg, "testchannel")
