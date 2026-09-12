2a5988c184d8f54f9822274c285fb92d2446182a
Hanabi <317387557+Hanabi9248@users.noreply.github.com>
2026-09-13 06:36:45 +0800

    integration: exercise Idemix after a V3_0 upgrade
    Extend the Idemix end-to-end test to upgrade the channel capability and reuse the existing identity for an invoke and query.
    
    Signed-off-by: Hanabi <317387557+Hanabi9248@users.noreply.github.com>an
    
    Signed-off-by: Hanabi <317387557+Hanabi9248@users.noreply.github.com>

--- integration/idemix/idemix_test.go
+++ integration/idemix/idemix_test.go
@@ -104,6 +104,14 @@ var _ = Describe("EndToEnd", func() {
 			InvokeWithIdemix(network, orderer, peer, idemixOrg, "testchannel")
 			QueryWithIdemix(network, peer, idemixOrg, "testchannel", "80")
 			Query(network, peer, "testchannel", "80")
+
+			By("upgrading the channel capability to V3_0")
+			nwo.EnableCapabilities(network, "testchannel", "Channel", "V3_0", orderer, network.Peer("Org1", "peer0"), network.Peer("Org2", "peer0"))
+
+			By("using the existing Idemix identity after the upgrade")
+			InvokeWithIdemix(network, orderer, peer, idemixOrg, "testchannel")
+			QueryWithIdemix(network, peer, idemixOrg, "testchannel", "70")
+			Query(network, peer, "testchannel", "70")
 		})
 	})
 })
